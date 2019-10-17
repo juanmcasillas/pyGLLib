f0696a3686cfa6e14ac029c238b6062c87260dc0
Juan M. Casillas <juanm.casillas@gmail.com>
2019-10-17 12:21:22 +0200

    added normals

--- gpx.py
+++ gpx.py
@@ -17,6 +17,51 @@ import os
 import math
 import pyGLLib
 
+
+def test_gpx(fname):
+
+    # a = [1, 1, 1, 
+    #      2, 8, 2, 
+    #      10, 10, 3,
+    #      15, 4, 9 ]
+
+    # x = BoundingBox()
+    # print(np.array(a).reshape(4,3))
+    # a = x.calculate(a, offset=False, interp=((0,150), (0,100), (0,1)))
+    # print("---")
+    # print(np.array(a).reshape(4,3))
+    # print(x)
+    # print(x.coords)
+
+    loader = GPXLoader(fname)
+    points = loader.load() 
+
+    #
+    # get the points, move then to the top left origin, and then,
+    # map the vectors to the new coordinate space.
+    #
+    # bb = BoundingBox()
+    # points = bb.calculate(points, offset=True, interp=((0,10), (0,10), (0,5)))
+    # print(points)
+    # import matplotlib.pyplot as plt
+    # d = np.array(points).reshape(int(len(points)/3),3)
+    # x = d[:,[0]]
+    # y = d[:,[1]]
+    # plt.plot(x,y)
+    # plt.show()
+
+    #
+    # get the points, move then to the top left origin, and then,
+    # map the vectors to the new coordinate space.
+    #
+    bb = BoundingBox()
+    points = bb.calculate(points, offset=True, interp=((0,10), (0,10), (0,5)))
+
+    roadgen = RoadGenerator(points)
+    vertex = roadgen.build(1)
+
+    draw(vertex)
+
 # ///////////////////////////////////////////////////////////////////////////
 #
 #
@@ -121,8 +166,10 @@ class BoundingBox(object):
         (minx, maxx, miny, maxy) members.
         """
         if isinstance(points,list):
-            points = np.array(points).reshape(int(len(points)/3) ,3)
-            
+            if not normals:            
+                points = np.array(points).reshape(int(len(points)/3) ,3)
+            else:
+                points = np.array(points).reshape(int(len(points)/6) ,6)
        
         self.minx = np.min(points[:,[0]]) # X
         self.maxx = np.max(points[:,[0]]) # X
@@ -135,7 +182,12 @@ class BoundingBox(object):
 
         # offset the data to 0 (top, left)
         if offset:
-            points = points - self.coords[0]
+            if normals:
+                x,y,z = self.coords[0]
+                points = points - [ x, y, z, 0.0, 0.0, 0.0]
+            else:
+                points = points - self.coords[0]
+
             self.minx_o = self.minx
             self.maxx_o = self.maxx
 
@@ -157,12 +209,25 @@ class BoundingBox(object):
         
         # interpolate the data to the new space
         if interp is not None:
-            x = np.interp(points[:,[0]], (self.minx, self.maxx), interp[0])
-            y = np.interp(points[:,[1]], (self.miny, self.maxy), interp[1])
-            z = np.interp(points[:,[2]], (self.minz, self.maxz), interp[2])
-            points = np.column_stack((x,y,z))
 
-        #return points.reshape(points.size)
+            #
+            # sighly open the range to avoid bad interpolation of decimals
+            # due precision           
+            #
+            D = 0.1
+            x = np.interp(points[:,[0]], (self.minx-D, self.maxx+D), interp[0])
+            y = np.interp(points[:,[1]], (self.miny-D, self.maxy+D), interp[1])
+            z = np.interp(points[:,[2]], (self.minz-D, self.maxz+D), interp[2])   
+            if normals:
+                a = points[:,[3]]
+                b = points[:,[4]]
+                c = points[:,[5]]
+                points = np.column_stack((x,y,z,a,b,c))
+            else:
+                points = np.column_stack((x,y,z))
+    
+  
+
         return points.flatten()
 
     @property
@@ -221,14 +286,14 @@ def V_U(U):
 class RoadGenerator(object):
     def __init__(self, points):
         # build a [len,3] matrix
-        self.points = np.array(points).reshape(int(len(points)/3),3)
         
+        self.bb = BoundingBox()
+        self.points = self.bb.calculate(points, offset=True, interp=((-1,1), (-1,1), (-1,1)))
+        self.points = np.array(self.points).reshape(int(len(self.points)/3),3)
     
     def add_perpendicular(self, distance):
         i = 0
         vertex = []
-        # don't forget the last one!!!
-
         for i in range(len(self.points)):
             if i == len(self.points)-1:
                 # last point
@@ -237,12 +302,17 @@ class RoadGenerator(object):
             else:
                 P = self.points[i]
                 Q = self.points[i+1]
+            
+            # remove the height for now
+            P[2] = Q[2] = 0.0
+            
             PQ = Q-P
-            PQ_U = V_U(PQ)
+            #PQ_U = V_U(PQ)
             T1 = np.array(( -PQ[1], PQ[0], 0)) * distance
             T2 = np.array(( PQ[1], -PQ[0], 0)) * distance
-            T1 = P + T1
-            T2 = P + T2
+            
+            T1 = P + T1 
+            T2 = P + T2 
             vertex += [ T1, P, T2]
             
         return vertex
@@ -256,6 +326,7 @@ class RoadGenerator(object):
         quads = self.add_perpendicular(distance)
         i=0
         # skip last polygon.
+      
         while i < len(quads)-5:
             Q = quads[i]
             R = quads[i+2]
@@ -273,7 +344,14 @@ class RoadGenerator(object):
 
             i = i+3
 
-        return(np.array(triangles).flatten())
+        x = np.array(triangles).flatten()
+        x = np.array(x).reshape(int(len(x)/6),6)
+        # swap Y by Z (data and normals)
+        x.T[[1, 2]] = x.T[[2, 1]]
+        x.T[[4, 5]] = x.T[[5, 4]]
+
+        x = self.bb.calculate(x, offset=True, interp=((-1,1), (-1,1), (-1,1)), normals=True)
+        return(np.array(x).flatten())
 
     def calc_normal_from_triangle(self, triangle):
         
@@ -284,17 +362,17 @@ class RoadGenerator(object):
         #first vertex
         QR = R-Q
         QS = S-Q
-        normals[0] = list(np.cross(QR,QS))
+        normals[0] =  V_U(list(np.cross(QR,QS)))
 
         #second vertex
         RS = S-R
         RQ = Q-R
-        normals[1] = list(np.cross(RS,RQ))
+        normals[1] = V_U(list(np.cross(RS,RQ)))
 
         #third vertex
         SQ = Q-S
         SR = R-S
-        normals[2] = list(np.cross(SQ,SR))
+        normals[2] = V_U(list(np.cross(SQ,SR)))
         return normals    
 
 # ///////////////////////////////////////////////////////////////////////////
@@ -310,49 +388,6 @@ def draw(points):
     plt.plot(x,y, linestyle="",marker="o")
     plt.show()    
 
-def test_gpx(fname):
-
-    # a = [1, 1, 1, 
-    #      2, 8, 2, 
-    #      10, 10, 3,
-    #      15, 4, 9 ]
-
-    # x = BoundingBox()
-    # print(np.array(a).reshape(4,3))
-    # a = x.calculate(a, offset=False, interp=((0,150), (0,100), (0,1)))
-    # print("---")
-    # print(np.array(a).reshape(4,3))
-    # print(x)
-    # print(x.coords)
-
-    loader = GPXLoader(fname)
-    points = loader.load() 
-
-    #
-    # get the points, move then to the top left origin, and then,
-    # map the vectors to the new coordinate space.
-    #
-    # bb = BoundingBox()
-    # points = bb.calculate(points, offset=True, interp=((0,10), (0,10), (0,5)))
-    # print(points)
-    # import matplotlib.pyplot as plt
-    # d = np.array(points).reshape(int(len(points)/3),3)
-    # x = d[:,[0]]
-    # y = d[:,[1]]
-    # plt.plot(x,y)
-    # plt.show()
-
-    #
-    # get the points, move then to the top left origin, and then,
-    # map the vectors to the new coordinate space.
-    #
-    bb = BoundingBox()
-    points = bb.calculate(points, offset=True, interp=((0,10), (0,10), (0,5)))
-
-    roadgen = RoadGenerator(points)
-    vertex = roadgen.build(1)
-
-    draw(vertex)
 
 class GLRoad(pyGLLib.object.GLObjectBaseNormal):
     
@@ -364,13 +399,11 @@ class GLRoad(pyGLLib.object.GLObjectBaseNormal):
     def load_model(self):
         loader = GPXLoader(self.fname)
         points = loader.load() 
-        bb = BoundingBox()
-        points = bb.calculate(points, offset=True, interp=((0,10), (0,10), (0,5)))
-
         roadgen = RoadGenerator(points)
         self.vertexData = roadgen.build(self.distance)
-        self.triangles = len(self.vertexData)/6
-
+        self.triangles = len(self.vertexData)/(3*6)
+        print(np.array(self.vertexData).reshape(int(len(self.vertexData)/6),6))
+ 
 
 if __name__ == "__main__":
     test_gpx(sys.argv[1])
